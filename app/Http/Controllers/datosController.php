86da1cd2ed5d40007976007e1a519707e1c2f31c
Santiago Aguirre <aguirresantiago81@gmail.com>
2020-11-30 12:10:58 -0300

    Commit inicial del proyecto. Creada una ruta y un controlador

---
+++ app/Http/Controllers/datosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class datosController extends Controller
+{
+    public function returnDatos()
+    {
+        return 'Aqui se mostraran los datos de la ddbb';
+    }
+}
